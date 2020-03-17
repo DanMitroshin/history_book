53d4fddfd55fc5f29a7669d249947753e876f1b3
Denis Mitroshin <alfocintavr@mail.ru>
2020-03-17 20:25:41 +0300

    Updates

---
+++ precache-manifest.4df06e54b7155a0f40261b2f6b70881c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98736bf39c6141673dcc0e3ead9d5de",
+    "url": "/history_book/index.html"
+  },
+  {
+    "revision": "e71c2db2c848c6f06793",
+    "url": "/history_book/static/css/main.68867fbe.chunk.css"
+  },
+  {
+    "revision": "c02f1c0cbf8758153caa",
+    "url": "/history_book/static/js/2.239ed03a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/history_book/static/js/2.239ed03a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71c2db2c848c6f06793",
+    "url": "/history_book/static/js/main.4e2da452.chunk.js"
+  },
+  {
+    "revision": "5943f39835c785ed6a09",
+    "url": "/history_book/static/js/runtime-main.be361dd9.js"
+  },
+  {
+    "revision": "59d60af3c22a11a2bf842cd9490afb2b",
+    "url": "/history_book/static/media/Circe-Bold.59d60af3.ttf"
+  },
+  {
+    "revision": "6c59e123427d03197dbcddd8e5506deb",
+    "url": "/history_book/static/media/Circe-Bold.6c59e123.eot"
+  },
+  {
+    "revision": "977dee27389906fcdb91575955cf543c",
+    "url": "/history_book/static/media/Circe-Bold.977dee27.woff"
+  },
+  {
+    "revision": "8736804787b67fa125b215f86d3b66bd",
+    "url": "/history_book/static/media/Circe-ExtraBold.87368047.woff"
+  },
+  {
+    "revision": "8d9ba51f3f1ecf7c58580ccc56adc490",
+    "url": "/history_book/static/media/Circe-ExtraBold.8d9ba51f.ttf"
+  },
+  {
+    "revision": "b325a34030a457c284f193c8c64bb1ef",
+    "url": "/history_book/static/media/Circe-ExtraBold.b325a340.eot"
+  },
+  {
+    "revision": "171fe7df080d14fe10ea9a6f33bf92b1",
+    "url": "/history_book/static/media/Circe-ExtraLight.171fe7df.woff"
+  },
+  {
+    "revision": "2ff6670b8281283051726cf25cb9b78b",
+    "url": "/history_book/static/media/Circe-ExtraLight.2ff6670b.eot"
+  },
+  {
+    "revision": "7fc3ecc069378d95e98f69482de84ae2",
+    "url": "/history_book/static/media/Circe-ExtraLight.7fc3ecc0.ttf"
+  },
+  {
+    "revision": "35b2348d354d083173d9b7dac3fd6523",
+    "url": "/history_book/static/media/Circe-Light.35b2348d.ttf"
+  },
+  {
+    "revision": "85c00974099ece870bd68c33da68d411",
+    "url": "/history_book/static/media/Circe-Light.85c00974.woff"
+  },
+  {
+    "revision": "de6af660eac77fdd3d5bcbbb67a115be",
+    "url": "/history_book/static/media/Circe-Light.de6af660.eot"
+  },
+  {
+    "revision": "7786e397b9971d35f3893a1dec2b1b3f",
+    "url": "/history_book/static/media/Circe-Regular.7786e397.eot"
+  },
+  {
+    "revision": "c7d081a3dfb9356de83ac79a28ec13d4",
+    "url": "/history_book/static/media/Circe-Regular.c7d081a3.ttf"
+  },
+  {
+    "revision": "e8c2bdb5866b55bb52b64b8ea8b4a38d",
+    "url": "/history_book/static/media/Circe-Regular.e8c2bdb5.woff"
+  },
+  {
+    "revision": "c8b6116e2255952ba80556d8c3ae0596",
+    "url": "/history_book/static/media/Circe-Thin.c8b6116e.ttf"
+  },
+  {
+    "revision": "ce6ea34285d46fc250e50bd72fdb4255",
+    "url": "/history_book/static/media/Circe-Thin.ce6ea342.woff"
+  },
+  {
+    "revision": "d9851189d1838977014feb34fb4eac8a",
+    "url": "/history_book/static/media/Circe-Thin.d9851189.eot"
+  }
+]);
