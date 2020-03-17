832b086bc1f4a6c6ef9dc67dbb9bac69636bc0e3
Denis Mitroshin <alfocintavr@mail.ru>
2020-03-17 20:54:07 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import MainRouter from "./routers";
 
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <MainRouter/>
-    </BrowserRouter>
+    </HashRouter>
 ,
 document.getElementById('root'));
 
